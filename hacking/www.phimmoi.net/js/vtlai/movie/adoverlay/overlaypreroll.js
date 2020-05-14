3e961afd78ad5f01086d6b1eaf408936500ced92
gaconkzk <gaconkzk@gmail.com>
2020-05-14 13:48:39 +0700

    giving up

---
+++ hacking/www.phimmoi.net/js/vtlai/movie/adoverlay/overlaypreroll.js
@@ -0,0 +1,6 @@
+if(typeof device!="object"){
+	document.write('<script src="/js/player/v3.0/tools/device.js"></script>');
+}
+document.write('<script src="/js/vtlai/movie/adoverlay/define-outside.js"></script>');
+document.write('<script src="/js/vtlai/movie/adoverlay/vastlist.js"></script>');
+document.write('<script src="/js/vtlai/movie/adoverlay/adcallback.js"></script>');
--- test/phimmoi-spec.js
+++ test/phimmoi-spec.js
@@ -8,3 +8,11 @@ test('phimmoi search videos', async t => {
   const videos = await phimmoi.search('kubo')
   t.is(videos.length, 1)
 })
+
+test.only('decode aes', async t => {
+  const url = 'U2FsdGVkX1\\/VqYDmwy6dyGC5TcL6HWNpBSiYyXEeJe9whGxVuDKoyeZv9PkaqhZwjXCoPcXxlodpNxeTa2GlfzI+vZqMguqc2jF9PTH+Y5E='
+  const pass = 'PhimMoi.Net@116213'
+  const pass2 = 'LQ4VM9HjX3xwcf1589364896e1780af3'
+
+  t.is(aes.dec(url, pass2), 'a')
+})
