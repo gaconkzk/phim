ce32829fcdb78ef7dafe14dd7a6951d7301046c8
gaconkzk <gaconkzk@gmail.com>
2020-05-21 09:03:25 +0700

    packaged

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = ['source-map'].map(devtool => ({
+  mode: 'development',
+  entry: './lib/server.js',
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'phimaser',
+    libraryTarget: 'umd'
+  },
+  devtool,
+  optimization: {
+    runtimeChunk: true
+  },
+  externals: ['fs']
+}))
